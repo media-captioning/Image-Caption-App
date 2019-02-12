930232f2462ffa5c257cfd2a05619a65bbf6ded6
seanfynn <axeinternationalauto@gmail.com>
2019-02-11 23:39:19 -0500

    finally committing

---
+++ appy2.py
@@ -0,0 +1,162 @@
+#our web app framework!
+
+#you could also generate a skeleton from scratch via
+#http://flask-appbuilder.readthedocs.io/en/latest/installation.html
+
+#Generating HTML from within Python is not fun, and actually pretty cumbersome because you have to do the
+#HTML escaping on your own to keep the application secure. Because of that Flask configures the Jinja2 template engine 
+#for you automatically.
+#requests are objects that flask handles (get set post, etc)
+from flask import Flask, render_template,request
+#scientific computing library for saving, reading, and resizing images
+from scipy.misc import imsave, imread, imresize
+#for matrix math
+import numpy as np
+#for importing our keras model
+import keras.models
+#for regular expressions, saves time dealing with string data
+import re
+
+#system level operations (like loading files)
+import sys 
+#for reading operating system data
+import os
+#tell our app where our saved model is
+sys.path.append(os.path.abspath("./model"))
+from load import * 
+#initalize our flask app
+app = Flask(__name__)
+#global vars for easy reusability
+global model, graph
+#initialize these variables
+model, graph = init()
+
+#decoding an image from base64 into raw representation
+def generate_caption(image_path, max_tokens=30):
+    """
+    Generate a caption for the image in the given path.
+    The caption is limited to the given number of tokens (words).
+    """
+
+    # Load and resize the image.
+    image = load_image(image_path, size=img_size)
+    
+    # Expand the 3-dim numpy array to 4-dim
+    # because the image-model expects a whole batch as input,
+    # so we give it a batch with just one image.
+    image_batch = np.expand_dims(image, axis=0)
+
+    # Process the image with the pre-trained image-model
+    # to get the transfer-values.
+    transfer_values = image_model_transfer.predict(image_batch)
+
+    # Pre-allocate the 2-dim array used as input to the decoder.
+    # This holds just a single sequence of integer-tokens,
+    # but the decoder-model expects a batch of sequences.
+    shape = (1, max_tokens)
+    decoder_input_data = np.zeros(shape=shape, dtype=np.int)
+
+    # The first input-token is the special start-token for 'ssss '.
+    token_int = token_start
+
+    # Initialize an empty output-text.
+    output_text = ''
+
+    # Initialize the number of tokens we have processed.
+    count_tokens = 0
+
+    # While we haven't sampled the special end-token for ' eeee'
+    # and we haven't processed the max number of tokens.
+    while token_int != token_end and count_tokens < max_tokens:
+        # Update the input-sequence to the decoder
+        # with the last token that was sampled.
+        # In the first iteration this will set the
+        # first element to the start-token.
+        decoder_input_data[0, count_tokens] = token_int
+
+        # Wrap the input-data in a dict for clarity and safety,
+        # so we are sure we input the data in the right order.
+        x_data = \
+        {
+            'transfer_values_input': transfer_values,
+            'decoder_input': decoder_input_data
+        }
+
+        # Note that we input the entire sequence of tokens
+        # to the decoder. This wastes a lot of computation
+        # because we are only interested in the last input
+        # and output. We could modify the code to return
+        # the GRU-states when calling predict() and then
+        # feeding these GRU-states as well the next time
+        # we call predict(), but it would make the code
+        # much more complicated.
+        
+        # Input this data to the decoder and get the predicted output.
+        decoder_output = decoder_model.predict(x_data)
+
+        # Get the last predicted token as a one-hot encoded array.
+        # Note that this is not limited by softmax, but we just
+        # need the index of the largest element so it doesn't matter.
+        token_onehot = decoder_output[0, count_tokens, :]
+
+        # Convert to an integer-token.
+        token_int = np.argmax(token_onehot)
+
+        # Lookup the word corresponding to this integer-token.
+        sampled_word = tokenizer.token_to_word(token_int)
+
+        # Append the word to the output-text.
+        output_text += " " + sampled_word
+
+        # Increment the token-counter.
+        count_tokens += 1
+
+    # This is the sequence of tokens output by the decoder.
+    output_tokens = decoder_input_data[0]
+
+    # Plot the image.
+    plt.imshow(image)
+    plt.show()
+    
+    # Print the predicted caption.
+    print("Predicted caption:")
+    print(output_text)
+    print()
+	
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/auth/google")
+def googleauth():
+    return '<h1> GOOGLElOGI</h1>'
+
+
+@app.route("/auth/facebook")
+def faceauth():
+    return '<h1> Facebook</h1>'
+
+@app.route("/auth/register")
+def register():
+    return '<h1> Facebook</h1>'
+
+
+@app.route('/predict/',methods=['GET','POST'])
+def predict():
+	#whenever the predict method is called, we're going
+	#to input the user drawn character as an image into the model
+	#perform inference, and return the classification
+	#get the raw data format of the image
+    image_path = request.get_data()
+
+    generate_caption(image_path )
+	#encode it into a suitable format
+    
+
+
+	
+
+if __name__ == "__main__":
+   app.run(debug=True)
